21994cbbf8d90d3d07cb1b3888f4e50232255d97
yangyang <yangmeansyoung@gmail.com>
2017-01-29 23:21:23 +1100

    Refactor use common "walk" fn

--- src/raml_clj_parser/core.clj
+++ src/raml_clj_parser/core.clj
@@ -1,32 +1,29 @@
 (ns raml-clj-parser.core
   (:require [raml-clj-parser.reader :as reader]
-            [raml-clj-parser.util :as util]
-
-            ))
+            [raml-clj-parser.util :as util]))
 
 (defn read-raml [path]
   (if-let  [[content base_url] (util/when-exist path)]
     (reader/read content base_url)
     {:error "file not exist"}))
 
-(defn- get-uri-parameters [raml path_keys ]
+;;FIXME: walk should be able do the same thing
+(defn- extract [ctor-fn raml path_keys]
   (if (empty? path_keys) ""
       (let [path     (first path_keys)
             resource (get raml path)]
+        (ctor-fn resource (extract ctor-fn resource (rest path_keys))))))
 
-        (into (get-in  resource [:raml-clj-parser.reader/uri-parameters]) (get-uri-parameters resource  (rest path_keys))))))
+(defn- uri-parameters [r nest_r] (into (get-in r [:raml-clj-parser.reader/uri-parameters]) nest_r))
 
-(defn- get-uri [raml path_keys]
-  (if (empty? path_keys) ""
-      (let [path     (first path_keys)
-            resource (get raml path)]
-        (str (get-in  resource [:uri]) (get-uri resource  (rest path_keys))))))
+(defn- uri [r nest_r] (str (get-in r [:uri]) nest_r))
 
 (defn get-abs-uri [raml keys_to_leaf]
   "will return a map with abs uri and uriParameters if presents"
   (let [base_uri            (get-in raml [:baseUri :uri])
         base_uri_parameters (get-in raml
                                  [:baseUri :raml-clj-parser.reader/uri-parameters])]
-    {:uri                                   (str base_uri (get-uri raml keys_to_leaf))
-     :raml-clj-parser.reader/uri-parameters (into base_uri_parameters (get-uri-parameters raml keys_to_leaf))}
-    ))
+    {:uri                                   (str base_uri (extract uri raml keys_to_leaf))
+     :raml-clj-parser.reader/uri-parameters (into base_uri_parameters
+
+                                                  (extract uri-parameters raml keys_to_leaf))}))
